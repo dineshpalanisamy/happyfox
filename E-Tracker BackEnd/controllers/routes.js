7db3671ff2929571a3e09f801692f596cff7aef5
CORP\dpalanisamy <tpdineshtp@gmail.com>
2017-05-23 00:14:12 +0530

    [Added \add_expense API]

--- E-Tracker BackEnd/controllers/routes.js
+++ E-Tracker BackEnd/controllers/routes.js
@@ -4,4 +4,24 @@ module.exports = function(app) {
 	app.get('/', function(req, res){
 	    res.end("Hello Happyfox");
 	});
+
+  app.post('/add_expense', function(req, res){
+      var title = req.body.title;
+      var amount = req.body.amount;
+      var notes = req.body.notes;
+      var catagory = req.body.catagory;
+
+      var newexpense = new model.Expense();
+      newexpense.title = title;
+      newexpense.amount = amount;
+      newexpense.notes = notes;
+      newexpense.catagory = catagory;
+
+      newexpense.save(function(err, savedExpense){
+        if(err){
+          console.log(err);
+        }
+        return res.status(200).send();
+      })
+  });
 }
